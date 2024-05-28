b3d7573691e277f7fd4d335d0e3885fc1ff7f2a7
rteoh25 <158127228+rteoh25@users.noreply.github.com>
2024-05-28 19:08:57 +0800

    Update libft.h

--- libft.h
+++ libft.h
@@ -23,13 +23,11 @@
 #  define BUFFER_SIZE 20
 # endif
 
-
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t len);
